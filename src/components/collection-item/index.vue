ac80c5c510820670308d7a90b42036d1933c47a9
黄建永 <huangjianyong@ezhong.com>
2022-05-27 00:36:34 +0800

    style: the page of collection and collection-item

---
+++ src/components/collection-item/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="collection-item-wrapper">
+    <img class="image" :src="itemInfo.imgUrl" alt="">
+    <div class="detail">
+      <p class="title ellipsis">{{ itemInfo.title }}</p>
+      <p class="id ellipsis">{{ itemInfo.id }}</p>
+      <p class="company ellipsis">{{ itemInfo.company }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "collection-item",
+  data(){
+    return {
+      // itemInfo:{
+      //   imgUrl:'https://w1-dev.oss-cn-hangzhou.aliyuncs.com/photos/digital/initial/produce-2%402x.png',
+      //   title:'518纪念徽章',
+      //   id:'AC554236#4421AC554236#4421AC554236#4421AC554236#4421AC554236#4421',
+      //   company:'杭州众力网络科技杭州众力网络科技杭州众力网络科技杭州众力网络科技',
+      // }
+    }
+  },
+  props:{
+    itemInfo:{
+      type:Object,
+      required: true
+    }
+  },
+  computed:{
+
+  },
+  created() {
+
+  },
+  methods(){
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.collection-item-wrapper{
+  background-color: #252726;
+  border-radius: 40rpx;
+
+  .image{
+    border-radius: 40rpx;
+    overflow: hidden;
+  }
+
+  .detail{
+    padding: 40rpx 26rpx;
+
+    .title{
+      font-weight: 600;
+      font-size: 24rpx;
+      line-height: 34rpx;
+    }
+
+    .id{
+      margin: 17rpx 0;
+      padding: 0 30rpx;
+      font-size: 20rpx;
+      line-height: 28rpx;
+      color: #CF703A;
+      background: #FFDE9E;
+      border-radius: 10rpx;
+    }
+
+    .company{
+      font-weight: 600;
+      font-size: 24rpx;
+      line-height: 34rpx;
+    }
+  }
+}
+</style>
--- src/pages/tabbar-pages/collection/index.vue
+++ src/pages/tabbar-pages/collection/index.vue
@@ -1,17 +1,143 @@
 <template>
 <div class="collection-page">
-  collection
+  <header class="header-wrapper">
+    <p class="header">我的展馆</p>
+    <span class="id">NO.00000000001</span>
+  </header>
+
+  <section class="collection-wrapper">
+    <header class="collection-header">
+      <span class="title">我的藏品</span>
+      <span class="count">1</span>
+      <uni-icons class="icon" color="#fff" custom-prefix="iconfont" type="icon-danpin" size="20"></uni-icons>
+      <span class="time">时间</span>
+    </header>
+    <ul class="collection-list">
+      <collection-item
+        v-for="(item,index) in collectionData"
+        :key="index"
+        :item-info="item"
+      />
+    </ul>
+  </section>
 </div>
 </template>
 
 <script>
+import { uniIcons } from '@dcloudio/uni-ui'
+import CollectionItem from "@/components/collection-item";
 export default {
-  name: "collection"
+  name: "collection",
+  components:{ uniIcons, CollectionItem },
+  data(){
+    return {
+      collectionData:[
+        {
+          imgUrl:'https://w1-dev.oss-cn-hangzhou.aliyuncs.com/photos/digital/initial/produce-2%402x.png',
+          title:'518纪念徽章',
+          id:'AC554236#4421AC554236#4421AC554236#4421AC554236#4421AC554236#4421',
+          company:'杭州众力网络科技杭州众力网络科技杭州众力网络科技杭州众力网络科技',
+        },
+        {
+          imgUrl:'https://w1-dev.oss-cn-hangzhou.aliyuncs.com/photos/digital/initial/produce-2%402x.png',
+          title:'518纪念徽章',
+          id:'AC554236#4421AC554236#4421AC554236#4421AC554236#4421AC554236#4421',
+          company:'杭州众力网络科技杭州众力网络科技杭州众力网络科技杭州众力网络科技',
+        },
+        {
+          imgUrl:'https://w1-dev.oss-cn-hangzhou.aliyuncs.com/photos/digital/initial/produce-2%402x.png',
+          title:'518纪念徽章',
+          id:'AC554236#4421AC554236#4421AC554236#4421AC554236#4421AC554236#4421',
+          company:'杭州众力网络科技杭州众力网络科技杭州众力网络科技杭州众力网络科技',
+        },
+        {
+          imgUrl:'https://w1-dev.oss-cn-hangzhou.aliyuncs.com/photos/digital/initial/produce-2%402x.png',
+          title:'518纪念徽章',
+          id:'AC554236#4421AC554236#4421AC554236#4421AC554236#4421AC554236#4421',
+          company:'杭州众力网络科技杭州众力网络科技杭州众力网络科技杭州众力网络科技',
+        },
+        {
+          imgUrl:'https://w1-dev.oss-cn-hangzhou.aliyuncs.com/photos/digital/initial/produce-2%402x.png',
+          title:'518纪念徽章',
+          id:'AC554236#4421AC554236#4421AC554236#4421AC554236#4421AC554236#4421',
+          company:'杭州众力网络科技杭州众力网络科技杭州众力网络科技杭州众力网络科技',
+        },
+        {
+          imgUrl:'https://w1-dev.oss-cn-hangzhou.aliyuncs.com/photos/digital/initial/produce-2%402x.png',
+          title:'518纪念徽章',
+          id:'AC554236#4421AC554236#4421AC554236#4421AC554236#4421AC554236#4421',
+          company:'杭州众力网络科技杭州众力网络科技杭州众力网络科技杭州众力网络科技',
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .collection-page{
+  overflow: auto;
+  /* TODO: 底部样式待解决 */
+  //margin-bottom: -135rpx;
+  //padding-bottom: 135rpx;
+  background: #323639;
+  .header-wrapper{
+    margin: 65rpx 32rpx 140rpx;
+    .header{
+      font-weight: 600;
+      font-size: 40rpx;
+      line-height: 56rpx;
+    }
 
+    .id{
+      font-weight: 600;
+      font-size: 20rpx;
+      line-height: 28rpx;
+      color: #888888;
+    }
+  }
+
+  .collection-wrapper{
+    border-radius: 40rpx 40rpx 0 0;
+    padding: 26rpx 32rpx;
+    background-color: #101211;
+
+    .collection-header{
+      display: flex;
+      align-items: center;
+      .title{
+        font-size: 36rpx;
+        line-height: 50rpx;
+      }
+
+      .count{
+        margin-right: auto;
+        margin-left: 20rpx;
+        width: 36rpx;
+        height: 36rpx;
+        border-radius: 50%;
+        padding: 5rpx;
+        background-color: #252726;
+        text-align: center;
+        font-weight: 600;
+        font-size: 20rpx;
+        line-height: 28rpx;
+      }
+
+      .time{
+        margin-left: 22rpx;
+        font-size: 32rpx;
+        line-height: 45rpx;
+      }
+    }
+
+    .collection-list{
+      display: grid;
+      grid-template-columns: 47% 47%;
+      grid-auto-rows: auto;
+      grid-gap: 3% 6%;
+      padding: 20rpx 0;
+    }
+  }
 }
 </style>
