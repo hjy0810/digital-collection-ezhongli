d6149a05805e6522491b6217532013ee4c48bef1
黄建永 <huangjianyong@ezhongli.com>
2022-05-31 00:21:41 +0800

    style: avatar popup and nickname dialog in the page of edit question: can't get nickname dialog's selector

--- src/pages/edit/index.vue
+++ src/pages/edit/index.vue
@@ -9,7 +9,7 @@
         </div>
       </li>
 
-      <li class="info-item">
+      <li class="info-item" @click="showInputDialog">
         <span class="label">昵称</span>
         <div class="right">
           <span>{{ nickName }}</span>
@@ -34,12 +34,40 @@
     </ul>
 
     <!--  头像 popup  -->
-    <popup :show="true"/>
+    <uni-popup
+      ref="popup"
+      background-color="#000"
+      @change="change"
+      safe-area
+      type="bottom"
+      :mask-click="false"
+    >
+      <p class="popup-content" @click="toTakePhoto">拍摄</p>
+      <p class="popup-content">
+        <uni-file-picker limit="5" file-mediatype="all">从手机相册选择</uni-file-picker>
+      </p>
+      <p class="popup-content" @click="closePopup">取消</p>
+    </uni-popup>
 
+    <!--  昵称弹窗  -->
+    <uni-popup
+      ref="inputDialog"
+      type="dialog"
+    >
+      <uni-popup-dialog
+        ref="inputClose"
+        mode="input"
+        title="修改昵称"
+
+        placeholder="支持2-16位中英文、数字"
+        @confirm="dialogInputConfirm"
+      />
+    </uni-popup>
   </div>
 </template>
 
 <script>
+// import uniPopup from '@/components/popup/popup'
 import { uniIcons, uniPopup, uniTransition  } from '@dcloudio/uni-ui'
 export default {
   name: "edit",
@@ -47,25 +75,54 @@ export default {
     return {
       avatarUrl:'https://w1-dev.oss-cn-hangzhou.aliyuncs.com/photos/digital/initial/pinkduck%402x.png',
       nickName:'藏家X1dwddefq',
-      describe:'TA还什么都没留下~'
+      describe:'TA还什么都没留下~',
     }
   },
-  components:{ uniIcons, uniPopup, uniTransition},
+  components:{ uniIcons, uniPopup, uniTransition },
   computed:{
 
   },
   created() {
-    // console.log(  uniIcons, uniPopup, )
-
+    // uni.showToast({
+    //   title: '标题',
+    //   duration: 2000
+    // });
   },
   methods:{
+    showPopup() {
+      this.$refs.popup.open()
+    },
+    closePopup() {
+      this.$refs.popup.close()
+    },
+    showInputDialog(){
+      this.$refs.inputDialog.open()
+
+    },
+    closeInputDialog(){
+      this.$refs.inputDialog.close()
+
+    },
+    dialogInputConfirm(val){
+      uni.showLoading({
+        title: '2秒后会关闭'
+      })
+
+      setTimeout(() => {
+        uni.hideLoading()
+        console.log(val)
+        this.nickName = val
+        // 关闭窗口后，恢复默认内容
+        this.$refs.inputDialog.close()
+      }, 2000)
+    },
+    toTakePhoto(){
+      this.$refs.popup.close()
+      console.log('take photo')
+    },
     change(e) {
       console.log('当前模式：' + e.type + ',状态：' + e.show);
     },
-    showPopup() {
-      const popupDom = this.$children.find(item => item.$refs.popup)
-      // popupDom.$refs.popup.open()
-    }
   }
 }
 </script>
@@ -109,17 +166,29 @@ export default {
     }
   }
 
+  /* 修改avatar popup */
   .popup-content {
     display: flex;
     align-items: center;
     justify-content: center;
-    padding: 15px;
-    height: 50px;
-    background-color: #fff;
-    .text {
-      font-size: 12px;
-      color: #333;
+    padding: 15rpx;
+    height: 100rpx;
+    background-color: #252726;
+    font-size: 32rpx;
+    line-height: 45rpx;
+    color: #fff;
+    &:nth-child(1){
+      margin-bottom: 2rpx;
     }
+    &:nth-child(2){
+      margin-bottom: 10rpx;
+    }
+  }
+
+  /* 修改nickname dialog */
+  /* TODO: 无法取到dialog选择器 */
+  .uni-popup-dialog{
+   color: red;
   }
 }
 </style>
